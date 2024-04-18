dbe91127ce693a0d54ba53dbd7946ac1d78c8eb2
Benjamin E. Schlegel <kontakt@benjaminschlegel.ch>
2024-04-18 15:07:07 +0200

    Update print.testresults.R

--- R/print.testresults.R
+++ R/print.testresults.R
@@ -7,19 +7,23 @@ print.testresult <- function(model,X2,df.v,by.var) {
   }
   # longest name
   longest.char = max(nchar(var.names))
-  cat(paste0(rep("-",28+longest.char),collapse = ""),"\n")
-  cat(paste0("Test for",paste0(rep(" ", longest.char-6), collapse = ""), "X2\tdf\tprobability"),"\n")
-  cat(paste0(rep("-",28+longest.char),collapse = ""),"\n")
-  cat(paste0("Omnibus",paste0(rep(" ", longest.char-5), collapse = ""), round(X2[1],digits=2),"\t",df.v[1],"\t",round(p.values[1],digits=2)))
+  cat(spaces(28+longest.char, "-"),"\n")
+  cat(paste0("Test for",spaces(longest.char-6), "X2",spaces(6),"df",spaces(6),"probability"),"\n")
+  cat(spaces(28+longest.char, "-"),"\n")
+  cat(paste0("Omnibus",spaces(longest.char-5), round(X2[1],digits=2),spaces(8-nchar(round(X2[1],digits=2))),df.v[1],spaces(8-nchar(df.v[1])),round(p.values[1],digits=2)))
   cat("\n")
   for(i in 1:length(var.names)){
     name = var.names[i]
-    cat(paste0(name,paste0(rep(" ", longest.char-nchar(name)+2), collapse = ""), round(X2[i+1],digits=2),"\t",df.v[i+1],"\t",round(p.values[i+1],digits=2),"\n"))
+    cat(paste0(name, spaces(longest.char-nchar(name)+2), round(X2[i+1],digits=2),spaces(8-nchar(round(X2[i+1],digits=2))),df.v[i+1],spaces(8-nchar(df.v[i+1])),round(p.values[i+1],digits=2),"\n"))
   }
-  cat(paste0(rep("-",28+longest.char),collapse = ""),"\n\n")
+  cat(spaces(28+longest.char, "-"),"\n\n")
   cat("H0: Parallel Regression Assumption holds\n")
   result.matrix = matrix(c(X2, df.v, p.values), ncol = 3)
   rownames(result.matrix) = c("Omnibus", var.names)
   colnames(result.matrix) = c("X2","df","probability")
   result.matrix
+}
+
+spaces = function(n, sign = " "){
+  paste0(rep(sign, n), collapse = "")
 }
